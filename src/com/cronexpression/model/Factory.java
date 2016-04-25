d95920263448c614bd5f6e890583f97f6048ed87
Philipp Daniels <philipp.daniels@gmail.com>
2016-04-25 21:46:57 +0200

    Extend Factory with methods for month

--- src/com/cronexpression/model/Factory.java
+++ src/com/cronexpression/model/Factory.java
@@ -45,6 +45,90 @@ public class Factory {
         return new WeekDay(value);
     }
 
+    /**
+     * @return Month instance for January
+     */
+    public static Month january() {
+        return Month(1);
+    }
+
+    /**
+     * @return Month instance for February
+     */
+    public static Month february() {
+        return Month(2);
+    }
+
+    /**
+     * @return Month instance for March
+     */
+    public static Month march() {
+        return Month(3);
+    }
+
+    /**
+     * @return Month instance for April
+     */
+    public static Month april() {
+        return Month(4);
+    }
+
+    /**
+     * @return Month instance for May
+     */
+    public static Month may() {
+        return Month(5);
+    }
+
+    /**
+     * @return Month instance for June
+     */
+    public static Month june() {
+        return Month(6);
+    }
+
+    /**
+     * @return Month instance for July
+     */
+    public static Month july() {
+        return Month(7);
+    }
+
+    /**
+     * @return Month instance for August
+     */
+    public static Month august() {
+        return Month(8);
+    }
+
+    /**
+     * @return Month instance for September
+     */
+    public static Month september() {
+        return Month(9);
+    }
+
+    /**
+     * @return Month instance for October
+     */
+    public static Month october() {
+        return Month(10);
+    }
+
+    /**
+     * @return Month instance for November
+     */
+    public static Month november() {
+        return Month(11);
+    }
+
+    /**
+     * @return Month instance for December
+     */
+    public static Month december() {
+        return Month(12);
+    }
+
     public static class Minute {
 
         private int value;
